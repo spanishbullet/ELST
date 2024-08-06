dc6dd9348f405c3fb74adf4e7397b754572293b1
Training <Training@DESKTOP-K980HBH>
2024-08-06 12:36:30 -0400

    Add project files.

---
+++ ELST/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ELST
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openLogToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            infoToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem1 = new ToolStripMenuItem();
+            dirTreeView = new TreeView();
+            checkedListBox1 = new CheckedListBox();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, aboutToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(4, 1, 0, 1);
+            menuStrip1.Size = new Size(1064, 26);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openLogToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "&File";
+            // 
+            // openLogToolStripMenuItem
+            // 
+            openLogToolStripMenuItem.Name = "openLogToolStripMenuItem";
+            openLogToolStripMenuItem.Size = new Size(157, 26);
+            openLogToolStripMenuItem.Text = "&Open Log";
+            openLogToolStripMenuItem.Click += openLogToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(157, 26);
+            exitToolStripMenuItem.Text = "&Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { infoToolStripMenuItem, aboutToolStripMenuItem1 });
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(55, 24);
+            aboutToolStripMenuItem.Text = "&Help";
+            // 
+            // infoToolStripMenuItem
+            // 
+            infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            infoToolStripMenuItem.Size = new Size(133, 26);
+            infoToolStripMenuItem.Text = "&Help";
+            infoToolStripMenuItem.Click += infoToolStripMenuItem_Click;
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            aboutToolStripMenuItem1.Size = new Size(133, 26);
+            aboutToolStripMenuItem1.Text = "&About";
+            aboutToolStripMenuItem1.Click += aboutToolStripMenuItem1_Click;
+            // 
+            // dirTreeView
+            // 
+            dirTreeView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dirTreeView.Location = new Point(0, 39);
+            dirTreeView.Margin = new Padding(2);
+            dirTreeView.Name = "dirTreeView";
+            dirTreeView.Size = new Size(225, 557);
+            dirTreeView.TabIndex = 1;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "TimeCreated SystemTime", "Drive Capacity", "Manufacturer", "Model", "Revision", "Serial Number", "Parent ID" });
+            checkedListBox1.Location = new Point(237, 41);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(212, 554);
+            checkedListBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1064, 607);
+            Controls.Add(checkedListBox1);
+            Controls.Add(dirTreeView);
+            Controls.Add(menuStrip1);
+            Font = new Font("Segoe UI", 9F);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "ELST Main Menu";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openLogToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem infoToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem1;
+        private TreeView dirTreeView;
+        private CheckedListBox checkedListBox1;
+    }
+}
---
+++ ELST/Form1.cs
@@ -0,0 +1,49 @@
+using System.IO;
+
+namespace ELST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This prototype software is aimed to show event logs in a readable format that allows the user to see different events side by side and in context.");
+        }
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tool not yet funcitonal.");
+        }
+
+        private void openLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListDirectory(dirTreeView, "D:\\Windows\\System32\\winevt\\Logs");
+        }
+
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirInfo));
+        }
+
+        private static TreeNode CreateDirectoryNode(DirectoryInfo dirInfo)
+        {
+            var dirNode = new TreeNode(dirInfo.FullName);
+            foreach (var dir in dirInfo.GetDirectories())
+                dirNode.Nodes.Add(CreateDirectoryNode(dir));
+            foreach (var file in dirInfo.GetFiles())
+                dirNode.Nodes.Add(new TreeNode(file.Name));
+            return dirNode;
+        }
+    }
+}
